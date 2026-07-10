7814cf4ef9f6802ac661daf8ce75348977eeea2b
Anveshious <anvesha105@gmail.com>
2026-07-10 22:00:44 +0530

    Bug Fixes

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+    "css.lint.unknownAtRules": "ignore",
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
